b373a55a25bf3e1dbf89f0caab9fbf578969dd88
Thiago Augusto de Andrade <131469022+Thiago-pcpr@users.noreply.github.com>
2023-09-20 19:35:36 -0300

    Update SistemaDePedidos.java

--- SistemaDePedidos.java
+++ SistemaDePedidos.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaDePedidos {
     public static void main(String[] args) {
+        List<Produto> produtos = new ArrayList<>();
 
+        Produto Spaguetti = new Produto("1","Spaguetti");
+        Produto Canelone = new Produto("2", "Canelone");
+        Produto Talharim = new Produto("3", "Talharim");
+
+        produtos.add(Spaguetti);
+        produtos.add(Canelone);
+        produtos.add(Talharim);
     }
 }
