4d2d95773368d6c7e5ecb0091d0fc8d6f8e04261
Thiago Augusto de Andrade <131469022+Thiago-pcpr@users.noreply.github.com>
2023-09-20 19:34:27 -0300

    Update Mercado.java

--- Mercado.java
+++ Mercado.java
@@ -1,6 +1,6 @@
 public class Mercado extends Cliente{
-    public Mercado(String nome_cliente, String endereco_cliente, String CNPJ_cliente) {
-        super(nome_cliente, endereco_cliente, CNPJ_cliente);
+    public Mercado(String nome_cliente, String endereco_cliente, String CNPJ_cliente, String tipo_cliente) {
+        super(nome_cliente, endereco_cliente, CNPJ_cliente, "Mercado");
     }
 
     String tipo() {
