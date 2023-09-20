589ba3e3c278a238b2f5ac2f8c57901a15a81c67
Thiago Augusto de Andrade <131469022+Thiago-pcpr@users.noreply.github.com>
2023-09-20 19:34:52 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,8 +1,30 @@
 public class Pedido {
-    public int numero_unico_pedido;
-    Cliente x = new Cliente();
-    public String CNPJ_pedido = x.CNPJ_cliente;
-    Produto y = new Produto();
-    public String forma_pedido = y.forma_produto;
-    public float quantidade_pedido;
+    private int numeroUnico_pedido;
+    private Cliente cliente_pedido;
+    private Produto produto_pedido;
+    private float quantidade_pedido;
+
+    public Pedido(int numeroUnico_pedido, Cliente cliente_pedido, Produto produto_pedido, float quantidade_pedido) {
+        this.numeroUnico_pedido = numeroUnico_pedido;
+        this.cliente_pedido = cliente_pedido;
+        this.produto_pedido = produto_pedido;
+        this.quantidade_pedido = quantidade_pedido;
+    }
+
+    // Métodos getters para acessar os atributos
+    public int getNumeroUnicoPedido() {
+        return numeroUnico_pedido;
+    }
+
+    public Cliente getCliente() {
+        return cliente_pedido;
+    }
+
+    public Produto getProduto() {
+        return produto_pedido;
+    }
+
+    public float getQuantidadePedido() {
+        return quantidade_pedido;
+    }
 }
