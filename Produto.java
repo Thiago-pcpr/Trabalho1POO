86e5fbf8c3f7af1ca9503a5e8fa28edc76ff5146
Thiago Augusto de Andrade <131469022+Thiago-pcpr@users.noreply.github.com>
2023-09-20 19:35:05 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -6,4 +6,12 @@ public class Produto {
         this.preco_kg_produto = preco_kg_produto;
         this.forma_produto = forma_produto;
     }
+
+    public String getForma_produto() {
+        return forma_produto;
+    }
+
+    public float getPreco_kg_produto() {
+        return preco_kg_produto;
+    }
 }
